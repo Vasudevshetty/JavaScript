00dedc17fb1beab72c18852fe955a7b3543fafbd
Vasudevshetty <vasudeepu2815@gmail.com>
2024-03-20 22:47:11 +0530

    Learnt closures and iife.

--- ticketBoolAirline.js
+++ ticketBoolAirline.js
@@ -68,3 +68,50 @@ const addTax = function (rate) {
 };
 
 console.log(addTax(0.23)(200));
+
+
+///////////////////////////////////////
+// Immediately Invoked Function Expressions (IIFE)
+const runOnce = function () {
+  console.log('This will never run again');
+};
+runOnce();
+
+// IIFE
+(function () {
+  console.log('This will never run again');
+  const isPrivate = 23;
+})();
+
+// console.log(isPrivate);
+
+(() => console.log('This will ALSO never run again'))();
+
+{
+  const isPrivate = 23;
+  var notPrivate = 46;
+}
+// console.log(isPrivate);
+console.log(notPrivate);
+
+
+///////////////////////////////////////
+// Closures
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+
+console.dir(booker);
+
+                          
