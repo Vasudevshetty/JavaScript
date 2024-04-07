60fdfe62e603557dec9430402d2a0b5cda9ddc23
Vasudevshetty <vasudeepu2815@gmail.com>
2024-04-07 15:11:30 +0530

    Added loan functionality to the bankist app, yet to add sort method, and it finsihes this small project, Timers and date functionailities will be added in subsequent sections.

--- Bankist/script.js
+++ Bankist/script.js
@@ -183,6 +183,18 @@ btnTransfer.addEventListener("click", function (e) {
   }
 });
 
+btnLoan.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const amount = +inputLoanAmount.value;
+
+  if (amount > 0 && currentAcc.movements.some((mov) => mov >= amount * 0.1)) {
+    currentAcc.movements.push(amount);
+    updateUI(currentAcc);
+  }
+  inputLoanAmount.value = "";
+});
+
 btnClose.addEventListener("click", function (e) {
   e.preventDefault();
 
