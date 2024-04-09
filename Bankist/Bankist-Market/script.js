6c1fe628f39dfaff6e4e9a76451464128b333003
Vasudevshetty <vasudeepu2815@gmail.com>
2024-04-09 14:07:41 +0530

    Added smooth scrolling for the 1st section.

--- Bankist/Bankist-Market/script.js
+++ Bankist/Bankist-Market/script.js
@@ -4,6 +4,12 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+const nav = document.querySelector(".nav");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
 
 const openModal = function (e) {
   e.preventDefault();
@@ -23,4 +29,35 @@ overlay.addEventListener("click", closeModal);
 
 document.addEventListener("keydown", function (e) {
   if (e.key === "Escape" && !modal.classList.contains("hidden")) closeModal();
+});
+
+/*
+const message = document.createElement("div");
+message.classList.add("cookie-message"); // Remove the dot (.)
+message.innerHTML =
+  "We use cookies for improved functionality and analytics." +
+  '<button class="btn btn--close-cookie">Got it!</button>';
+const header = document.querySelector(".header");
+header.prepend(message);
+message.style.backgroundColor = "#37383d";
+message.style.width = "120%"; // Corrected the width value
+
+const btnCloseCookie = document.querySelector(".btn--close-cookie");
+
+btnCloseCookie.addEventListener("click", () => message.remove());
+*/
+
+btnScrollTo.addEventListener("click", function (e) {
+  const s1coords = section1.getBoundingClientRect();
+
+  // window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset);
+
+  //oldschool way of doing it
+  /*window.scrollTo({
+    left: s1coords.left + window.pageXOffset,
+    top: s1coords.top + window.pageYOffset,
+    behavior: "smooth",
+  }); */
+
+  section1.scrollIntoView({ behavior: "smooth" });
 });
