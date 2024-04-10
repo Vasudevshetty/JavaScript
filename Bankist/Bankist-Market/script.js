a2e38c4d5fbf5d0b349062516eaad5ec88adc949
Vasudevshetty <vasudeepu2815@gmail.com>
2024-04-10 17:16:15 +0530

    Added sliders, and completed this small project once again, oops and async javascript has to be learnt asap.

--- Bankist/Bankist-Market/script.js
+++ Bankist/Bankist-Market/script.js
@@ -224,4 +224,75 @@ const imgObserver = new IntersectionObserver(lazyLoad, {
 
 imgTargets.forEach((img) => imgObserver.observe(img));
 
+// slidersss.
+const slides = document.querySelectorAll(".slide");
+const btnLeft = document.querySelector(".slider__btn--left");
+const btnRight = document.querySelector(".slider__btn--right");
+const dotContainer = document.querySelector(".dots");
 
+let currSlide = 0,
+  maxSlides = slides.length;
+
+const createDot = function () {
+  slides.forEach((_, index) => {
+    dotContainer.insertAdjacentHTML(
+      "beforeend",
+      `<button class="dots__dot" data-slide="${index}"></button>`
+    );
+  });
+};
+
+// support functions
+const activateDot = function (slide) {
+  document
+    .querySelectorAll(".dots__dot")
+    .forEach((dot) => dot.classList.remove("dots__dot--active"));
+
+  document
+    .querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add("dots__dot--active");
+};
+
+const goToSlide = function (slideNo) {
+  slides.forEach(
+    (slide, index) =>
+      (slide.style.transform = `translateX(${100 * (index - slideNo)}%)`)
+  );
+  activateDot(slideNo);
+};
+
+const nextSlide = function () {
+  if (currSlide === maxSlides - 1) currSlide = 0;
+  else currSlide++;
+
+  goToSlide(currSlide);
+};
+
+const prevSlide = function () {
+  if (currSlide === 0) currSlide = maxSlides - 1;
+  else currSlide--;
+
+  goToSlide(currSlide);
+};
+
+// event handlers
+btnRight.addEventListener("click", nextSlide);
+btnLeft.addEventListener("click", prevSlide);
+
+document.addEventListener("keydown", function (e) {
+  e.key === "ArrowRight" && nextSlide();
+  e.key === "ArrowLeft" && prevSlide();
+});
+
+dotContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("dots__dot")) {
+    goToSlide(e.target.dataset.slide);
+    activateDot(e.target.dataset.slide);
+  }
+});
+
+const init = function () {
+  createDot();
+  goToSlide(0);
+};
+init();
