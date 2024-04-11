6c1a611775bd283b4942fe242c2b8cc433f27e16
Vasudevshetty <vasudeepu2815@gmail.com>
2024-04-11 18:48:55 +0530

    Understood few things about how oops work in javascript got some good idea how prototype works, the protoypal inheritance and chaining of it.

--- OOPpractice.js
+++ OOPpractice.js
@@ -4,9 +4,10 @@ const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
 
-    this.calcAge() = function () {
-        
-    }
+  // this is bad practice
+  // this.calcAge() = function () {
+
+  // }
 };
 
 // new procedure
@@ -22,3 +23,51 @@ const visu = new Person("visu", 2005);
 
 console.log(vasu, visu);
 console.log(vasu instanceof Person);
+
+// instead create fnction this way using prototype which itself a propert
+// this is a prototypal inheritance, it avoids rewriting the calcAge function for every instance.
+Person.prototype.calcAge = function () {
+  return 2024 - this.birthYear;
+};
+
+console.log(Person.prototype);
+
+console.log(vasu.calcAge());
+
+console.log(vasu.__proto__);
+console.log(vasu.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(vasu));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// prototype can even contain properties not just methods.
+Person.prototype.species = "Homo Sapiens";
+
+console.log(vasu, visu);
+
+console.log(vasu.hasOwnProperty("species"));
+console.log(vasu.hasOwnProperty("firstName"));
+
+// Person.prototype
+console.log(vasu.__proto__);
+// Object.prtotype
+console.log(vasu.__proto__.__proto__);
+
+// null
+console.log(vasu.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+// Arrays
+const arr = [1, 2, 3, 4, 3, 4, 5];
+console.log(arr.__proto__.constructor);
+
+/* it's just a fun experiment, shouldn't prefer doing this. */
+// to make it intersting, lets create a property that all the arrays get
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+// check the prototypal level of few of the elements of the DOM, it would be really fun
